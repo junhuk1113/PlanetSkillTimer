9d1bb31d3af1643be5fc001fdc2c966497dbff6a
junhuk1113 <31131485+junhuk1113@users.noreply.github.com>
2024-02-25 11:32:02 +0900

    포지 설정버튼 지원 추가

--- forge/src/main/java/net/pmkjun/planetskilltimer/forge/PlanetSkillTimerForge.java
+++ forge/src/main/java/net/pmkjun/planetskilltimer/forge/PlanetSkillTimerForge.java
@@ -4,13 +4,20 @@ import net.pmkjun.planetskilltimer.PlanetSkillTimer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.pmkjun.planetskilltimer.forge.input.KeyMappings;
+import net.pmkjun.planetskilltimer.common.config.ConfigScreen;
 
 @Mod(PlanetSkillTimer.MOD_ID)
 public class PlanetSkillTimerForge {
     public PlanetSkillTimerForge() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(this::setup);
+
         KeyMappings keyMappings = new KeyMappings();
         keyMappings.register();
 
         PlanetSkillTimer.init();
+    }
+    private void setup(final FMLCommonSetupEvent event){
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) -> new ConfigScreen(screen)));
     }
 }
