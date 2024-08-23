30faeb3bfd4a3fa2b1d3e4b161a12c94c778e1ac
junhuk1113 <junhuk1113@naver.com>
2024-08-23 13:41:43 +0000

    보스바 감지 방식 변경(루나클지원)(1.4.3배포)

--- common/src/main/java/net/pmkjun/planetskilltimer/mixin/bossbarMixin.java
+++ common/src/main/java/net/pmkjun/planetskilltimer/mixin/bossbarMixin.java
@@ -1,11 +1,14 @@
 package net.pmkjun.planetskilltimer.mixin;
 
-import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.BossBarHud;
-import net.minecraft.entity.boss.BossBar;
-//import net.pmkjun.planetskilltimer.file.Skill;
+import net.minecraft.client.gui.hud.ClientBossBar;
+import net.minecraft.network.packet.s2c.play.BossBarS2CPacket;
+import net.minecraft.text.Text;
 import net.pmkjun.planetskilltimer.file.Stat;
-//import net.pmkjun.planetskilltimer.util.SkillLevel;
+
+import java.util.Map;
+import java.util.UUID;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,47 +17,54 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BossBarHud.class)
 public class bossbarMixin {
+    @Inject(method = "handlePacket", at={@At("RETURN")})
+    public void render(BossBarS2CPacket packet, CallbackInfo ci){
+        MinecraftClient mc = MinecraftClient.getInstance();
+        //mc.player.sendMessage(Text.literal("handlePacket 함수 실행됨"));
+        BossBarHud bossBarHud = mc.inGameHud.getBossBarHud();
+        Map<UUID, ClientBossBar> bossBars = ((BossBarHudAccessor) bossBarHud).getBossBars();
 
-
-    @Inject(method = "Lnet/minecraft/client/gui/hud/BossBarHud;renderBossBar(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/entity/boss/BossBar;)V",at = {@At("RETURN")})
-    private void bossbarmixin(DrawContext context, int x, int y, BossBar bossBar,CallbackInfo cir){
-        String bossbarText = bossBar.getName().getString();
-        String temp;
-        if(bossbarText.contains("%)")){
-            for (int i = 0; i < Stat.list.length ; i++)
-            {
-                if(bossbarText.contains(Stat.list[i])){
-                    temp=bossbarText.substring(bossbarText.indexOf(Stat.list[i]) +Stat.list[i].length()+1,bossbarText.indexOf("(")-1);
-                    try {
-                        if(Integer.parseInt(temp) > Stat.level[i])
-                        {
-                            Stat.level[i] = Integer.parseInt(temp);
-                            //System.out.println(Stat.list[i]+"의 레벨이 "+ temp + "(으)로 상승했습니다!");
-                            //System.out.println(Skill.list[i]+"의 지속시간은 "+SkillLevel.getActivateTime(i,Stat.level[i])/(double)1000+"초 입니다.");
-                            //System.out.println(Skill.list[i]+"의 쿨타임은 "+SkillLevel.getCooldownTime(i,Stat.level[i])/(double)1000+"초 입니다.");
+        for (Map.Entry<UUID, ClientBossBar> entry : bossBars.entrySet()) {
+            ClientBossBar bossBar = entry.getValue();
+            if (mc.player != null&&bossBar!=null) {
+                //mc.player.sendMessage(Text.literal("BossBar Text: " + bossBar.getName().getString()), false);
+                String bossbarText = bossBar.getName().getString();
+                String temp;
+                if(bossbarText.contains("%)")){
+                    for (int i = 0; i < Stat.list.length ; i++)
+                    {
+                        if(bossbarText.contains(Stat.list[i])){
+                            mc.player.sendMessage(Text.literal(bossbarText), false);
+                            temp=bossbarText.substring(bossbarText.indexOf(Stat.list[i]) +Stat.list[i].length()+1,bossbarText.indexOf("(")-1);
+                            if(Integer.parseInt(temp) > Stat.level[i])
+                            {
+                                Stat.level[i] = Integer.parseInt(temp);
+                                //mc.player.sendMessage(Text.literal(Stat.list[i] + "의 레벨이 " + temp + "(으)로 상승했습니다!"), false);
+                                //mc.player.sendMessage(Text.literal(Skill.list[i] + "의 지속시간은 " + SkillLevel.getActivateTime(i, Stat.level[i]) / (double) 1000 + "초 입니다."), false);
+                                //mc.player.sendMessage(Text.literal(Skill.list[i] + "의 쿨타임은 " + SkillLevel.getCooldownTime(i, Stat.level[i]) / (double) 1000 + "초 입니다."), false);
+                            }
                         }
-                    } catch (NumberFormatException e) {
-                        System.out.println("스킬 레벨 변환 실패! 개발자에게 문의하세요");
+                    }
+                }
+                else if(bossbarText.contains("최대치)")) {
+                    for (int i = 0; i < Stat.list.length; i++) {
+                        if (bossbarText.contains(Stat.list[i])) {
+                            temp = bossbarText.substring(bossbarText.indexOf(Stat.list[i]) +Stat.list[i].length()+1, bossbarText.indexOf("(") - 1);
+                            if (Integer.parseInt(temp) > Stat.level[i]) {
+                                Stat.level[i] = Integer.parseInt(temp);
+                                //mc.player.sendMessage(Text.literal(Stat.list[i] + "의 레벨이 " + temp + "(으)로 상승했습니다!"), false);
+                                //mc.player.sendMessage(Text.literal(Skill.list[i] + "의 지속시간은 " + SkillLevel.getActivateTime(i, Stat.level[i]) / (double) 1000 + "초 입니다."), false);
+                                //mc.player.sendMessage(Text.literal(Skill.list[i] + "의 쿨타임은 " + SkillLevel.getCooldownTime(i, Stat.level[i]) / (double) 1000 + "초 입니다."), false);
+                            
+                            }
+                        }
                     }
                 }
             }
-        }
-        else if(bossbarText.contains("최대치)")) {
-            for (int i = 0; i < Stat.list.length; i++) {
-                if (bossbarText.contains(Stat.list[i])) {
-                    temp = bossbarText.substring(bossbarText.indexOf(Stat.list[i]) +Stat.list[i].length()+1, bossbarText.indexOf("(") - 1);
-                    try {
-                        if(Integer.parseInt(temp) > Stat.level[i])
-                        {
-                            Stat.level[i] = Integer.parseInt(temp);
-                            //System.out.println(Stat.list[i]+"의 레벨이 "+ temp + "(으)로 상승했습니다!");
-                            //System.out.println(Skill.list[i]+"의 지속시간은 "+SkillLevel.getActivateTime(i,Stat.level[i])/(double)1000+"초 입니다.");
-                            //System.out.println(Skill.list[i]+"의 쿨타임은 "+SkillLevel.getCooldownTime(i,Stat.level[i])/(double)1000+"초 입니다.");
-                        }
-                    } catch (NumberFormatException e) {
-                        System.out.println("스킬 레벨 변환 실패! 개발자에게 문의하세요");
-                    }
-                }
+            else if(bossBar == null) 
+            {
+                //mc.player.sendMessage(Text.literal("bossBar is NULL!"));
+                continue;
             }
         }
     }
