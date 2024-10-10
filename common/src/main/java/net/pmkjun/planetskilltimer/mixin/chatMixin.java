d28798b3f2240760c76a25614609644158e61113
junhuk1113 <junhuk1113@naver.com>
2024-10-10 13:45:21 +0000

    modmenu

--- common/src/main/java/net/pmkjun/planetskilltimer/mixin/chatMixin.java
+++ common/src/main/java/net/pmkjun/planetskilltimer/mixin/chatMixin.java
@@ -1,7 +1,6 @@
 package net.pmkjun.planetskilltimer.mixin;
 
 import net.minecraft.client.GuiMessageTag;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MessageSignature;
@@ -9,20 +8,13 @@ import net.pmkjun.planetskilltimer.PlanetSkillTimerClient;
 import net.pmkjun.planetskilltimer.file.Skill;
 
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatComponent.class)
 public abstract class chatMixin {
-
-
-
-	@Shadow @Final private Minecraft client;
-
 	@Inject(method = "addMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/MessageSignature;ILnet/minecraft/client/GuiMessageTag;Z)V",at = @At(value = "RETURN", ordinal = 0))
 	private void addMessageMixin(Component message, @Nullable MessageSignature signature, int ticks, @Nullable GuiMessageTag indicator, boolean refresh, CallbackInfo ci) {
 
